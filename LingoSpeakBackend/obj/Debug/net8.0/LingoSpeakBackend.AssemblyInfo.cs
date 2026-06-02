7ce1b4c378ef684cb7bbfcf7302199a862382d12
Thai Phan Thanh Sang <sang.thaiphan.cit22@eiu.edu.vn>
2026-06-02 11:54:10 +0700

    update

--- LingoSpeakBackend/obj/Debug/net8.0/LingoSpeakBackend.AssemblyInfo.cs
+++ LingoSpeakBackend/obj/Debug/net8.0/LingoSpeakBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LingoSpeakBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddacf366a09c149ebc20fc16da05a5688a9905b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746811c036cef852360a739d8a60cc9339cccca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LingoSpeakBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LingoSpeakBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
